df8fbb6061be1c513bddce0d7607fabca68ce992
0ung <gupo941020@naver.com>
2025-12-09 11:13:11 +0900

    [ADD] CI/CD TEST

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -27,7 +27,7 @@ function App() {
       </div>
 
       <footer className="absolute bottom-4 text-white/50 text-sm">
-        Dev Server Environment
+        Dev Server Environment Dev Server Environment Dev Server Environment
       </footer>
     </div>
   );
