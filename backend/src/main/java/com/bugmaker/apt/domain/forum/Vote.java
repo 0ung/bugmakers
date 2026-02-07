7f230f3b1dba1df2f7eccff0be8a0c767d937887
0ung <gupo941020@naver.com>
2026-02-07 17:14:23 +0900

    [FIX] applicaiton 수정

--- backend/src/main/java/com/bugmaker/apt/domain/forum/Vote.java
+++ backend/src/main/java/com/bugmaker/apt/domain/forum/Vote.java
@@ -2,27 +2,34 @@ package com.bugmaker.apt.domain.forum;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.Comment;
 
+/**
+ * 투표 마스터 테이블
+ */
 @Entity
 @Table(name = "vote")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
-@Comment("투표 마스터 테이블")
 public class Vote {
+    /**
+     * 투표 ID
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Comment("투표 ID")
     private Long id;
 
+    /**
+     * 투표 항목명
+     */
     @Column(nullable = false, length = 100)
-    @Comment("투표 항목명")
     private String name;
 
+    /**
+     * 토론 ID
+     */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "forum_id", nullable = false)
-    @Comment("토론 ID")
     private Forum forum;
 }
